99ac9c9496bc1cddf6cb31b3827f6445b9cd95fc
Aimery Tauveron <atauveron@via.ecp.fr>
2019-04-14 15:04:29 +0200

    [RNN] Add basic RNN

---
+++ rnn.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import torch
+import torch.nn as nn
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_categories):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.in_to_hidden = nn.Linear(
+            n_categories + input_size + hidden_size, hidden_size)
+        self.in_to_out = nn.Linear(
+            n_categories + input_size + hidden_size, output_size)
+        self.out_to_out = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.in_to_hidden(input_combined)
+        output = self.in_to_out(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.out_to_out(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
